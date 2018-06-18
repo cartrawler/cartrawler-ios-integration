6fbb47e2b08bc7cd10aec0bc5cdbbcc8c8158d17
Natasha Cole <ncole@cartrawler.com>
2018-06-18 10:13:26 +0100

    Indentation.

--- CarTrawlerSDKIntegration/AppDelegate.swift
+++ CarTrawlerSDKIntegration/AppDelegate.swift
@@ -16,8 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         let primaryColor = UIColor(red: 3.0/255.0, green: 56.0/255.0, blue: 141.0/255.0, alpha: 1.0)
-	    let secondaryColor = UIColor(red: 3.0/255.0, green:56.0/255.0, blue:141.0/255.0, alpha: 1.0)
-	    let accentColor = UIColor(red: 241.0/255.0, green: 201.0/255.0, blue: 51.0/255.0, alpha: 1.0)
+        let secondaryColor = UIColor(red: 3.0/255.0, green:56.0/255.0, blue:141.0/255.0, alpha: 1.0)
+        let accentColor = UIColor(red: 241.0/255.0, green: 201.0/255.0, blue: 51.0/255.0, alpha: 1.0)
 
         let style = CTStyle.init(primaryColor: primaryColor, secondaryColor: secondaryColor, accentColor: accentColor, regularFont: nil, boldFont: nil, italicFont: nil)
         CarTrawlerSDK.sharedInstance().initialiseSDK(with: style, customParameters: nil, production: false)
